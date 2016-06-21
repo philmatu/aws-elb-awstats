8eb834070cf65e6379cea6a91fb51fafb4258881
philmatu <philip.matuskiewicz@nyct.com>
2016-06-21 14:18:32 +0000

    update script

--- elb/local_update.sh
+++ elb/local_update.sh
@@ -5,4 +5,5 @@ if [ ! -f /root/aws-elb-awstats/elb/localupdatelock ]; then
 	/usr/bin/python3 /root/aws-elb-awstats/elb/elb_compress.py /root/aws-elb-awstats/elb/config.ini
 	/usr/bin/python3 /root/aws-elb-awstats/elb/awstatsUpdater.py /root/aws-elb-awstats/elb/config.ini 
 	/bin/rm /root/aws-elb-awstats/elb/localupdatelock
+	/usr/share/awstats/tools/buildstatic.sh
 fi
